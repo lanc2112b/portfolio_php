193d7c1f8f4739206af8a547dd303ece7ae80d2f
Tony Lancaster <lanc1968@gmail.com>
2023-04-10 13:43:13 +0100

    Add basic routes -> routing table

--- public/index.php
+++ public/index.php
@@ -1,3 +1,20 @@
 <?php 
 
-echo "Hello";
+require '../Core/Router.php';
+
+$router = new Router();
+
+
+$router->add('',['controller' => 'Home', 'action' => 'index']);
+$router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
+$router->add('posts/new', ['controller' => 'Posts', 'action' => 'new']);
+
+
+echo '<pre>';
+var_dump($router->getRoutes());
+echo '</pre>';
+
+
+//echo get_class($router);
+//echo 'Requested URL: "' . $_SERVER['QUERY_STRING'] . '"'; 
+//echo "Hello";
