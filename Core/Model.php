bff8bde69f89f68c9426ec6254cd3a2bf5ef1135
Tony Lancaster <lanc1968@gmail.com>
2023-04-10 21:01:06 +0100

    Add Core Model class (abstract). Completes #3

---
+++ Core/Model.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Core;
+
+/** 
+ * Abstract DB Class (Core)
+*/
+
+use PDO;
+
+abstract class Model
+{
+
+    protected static function getDB()
+    {
+        static $db = null;
+
+        if ($db === null) {
+
+            try {
+
+                $dsn = 'mysql:host=' . $_ENV['DB_HOST'] . ';dbname=' . $_ENV['DB_NAME'] . ';charset=utf8';
+
+                $db = new PDO($dsn, $_ENV['DB_USER'], $_ENV['DB_PASS']);
+            } catch (\PDOException $e) {
+                echo $e->getMessage();
+            }
+        }
+
+        return $db;
+    }
+}
