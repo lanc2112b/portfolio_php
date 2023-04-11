c868b7f0b49dcbef6f5a3394852bd0ba12b284ea
Tony Lancaster <lanc1968@gmail.com>
2023-04-11 15:07:42 +0100

    Add test(s).

--- tests/unit/CoreRouterTest.php
+++ tests/unit/CoreRouterTest.php
@@ -14,4 +14,25 @@ class CoreRouterTest extends TestCase
 
         $this->assertSame('thisIsCamelCase', $camelCase);
     }
+
+    public function testConvertToStudlyCase()
+    {
+        $router = new Router();
+        $kebabCase = 'this-should-be-studly-case';
+
+        $studlyCase = $router->converToStudlyCaps($kebabCase);
+
+        $this->assertSame('ThisShouldBeStudlyCase', $studlyCase);
+    }
+
+    /* public function testRemoveQueryStringVariables()
+    {
+        $router = new Router();
+        $url = '/some/path/somewhere?something=where&other=else';
+
+        $cleanedUp = $router->removeQueryStringVariables($url);
+
+        $this->assertSame('/some/path/somewhere', $cleanedUp);
+        //removeQueryStringVariables()
+    } */
 }
