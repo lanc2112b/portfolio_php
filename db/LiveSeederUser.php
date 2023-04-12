3a64175228d08e916063c1624300c0ded5228432
Tony Lancaster <lanc1968@gmail.com>
2023-04-12 17:55:39 +0100

    Add tests for LiveSeederUser methods. Add Methods to LiveSeederUSer. Creates users table.

---
+++ db/LiveSeederUser.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace db;
+
+use Core\Model;
+use PDO;
+
+class LiveSeederUser extends Model
+{
+
+    public function dropTable()
+    {
+        $db = static::getDB();
+        $stmt = $db->query("DROP TABLE IF EXISTS users");
+        if ($stmt->execute()) {
+            return true;
+        }
+        return false;
+    }
+
+    public function createUsersTable()
+    {
+        $db = static::getDB();
+
+        $sql = "CREATE TABLE IF NOT EXISTS users(
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            gid INT NOT NULL DEFAULT 0,
+            is_admin INT NOT NULL DEFAULT 0,
+            first_name VARCHAR(50) NOT NULL,
+            last_name VARCHAR(50) NOT NULL,
+            photo_url TINYTEXT,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            updated_at TIMESTAMP NOT NULL ON UPDATE CURRENT_TIMESTAMP,
+            deleted_at TIMESTAMP,
+            refresh_at TIMESTAMP,
+            access_token TEXT
+            )";
+
+        $stmt = $db->query($sql);
+
+        if ($stmt->execute()) {
+            return true;
+        }
+        return false;
+    }
+
+    public function describeTable()
+    {
+        $db = static::getDB();
+
+        $stmt = $db->prepare("DESCRIBE users");
+
+        $stmt->execute();
+
+        $results = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+
+        return $results;
+    }
+
+
+}
---
+++ tests/integration/LiveSeederUserTest.php
@@ -0,0 +1,49 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use \db\LiveSeederUser;
+
+class LiveSeederUserTest extends TestCase
+{
+    public function testDropTable()
+    {
+        $seeder = new LiveSeederUser;
+
+        $dropped = $seeder->dropTable();
+
+        $this->assertEquals(true, $dropped);
+    }
+
+    public function testCreateTable()
+    {
+        $seeder = new LiveSeederUser;
+
+        $created = $seeder->createUsersTable();
+
+        $this->assertEquals(true, $created);
+    }
+
+    public function testTableHasColumns()
+    {
+        $seeder = new LiveSeederUser;
+
+        $columns = $seeder->describeTable();
+
+        $this->assertIsArray($columns);
+        $this->assertCount(11, $columns);
+        $this->assertArrayHasKey('Field', $columns[0]);
+        $this->assertEquals('id', $columns[0]['Field']);
+        $this->assertEquals('gid', $columns[1]['Field']);
+        $this->assertEquals('is_admin', $columns[2]['Field']);
+        $this->assertEquals('first_name', $columns[3]['Field']);
+        $this->assertEquals('last_name', $columns[4]['Field']);
+        $this->assertEquals('photo_url', $columns[5]['Field']);
+        $this->assertEquals('created_at', $columns[6]['Field']);
+        $this->assertEquals('updated_at', $columns[7]['Field']);
+        $this->assertEquals('deleted_at', $columns[8]['Field']);
+        $this->assertEquals('refresh_at', $columns[9]['Field']);
+        $this->assertEquals('access_token', $columns[10]['Field']);
+    }
+
+
+}
