56e536d1004cf48de36deecc2cd81d28a05e673e
Tony Lancaster <lanc1968@gmail.com>
2023-04-13 11:48:34 +0100

    Refactor method getAll. Add method getItemByID.

--- App/Models/Portfolio.php
+++ App/Models/Portfolio.php
@@ -2,18 +2,36 @@
 
 namespace App\Models;
 
+use PDO;
+
 class Portfolio extends \Core\Model
 {
 
     public function getAll()
     {
 
+        $sql = "SELECT * 
+                FROM portfolio_items";
+
         $db = static::getDB();
 
-        $stmt = $db->query("SELECT * FROM portfolio_items");
+        $stmt = $db->query($sql);
 
-        $results = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-        return $results;
+    }
+
+    public function getItembyID($id)
+    {
+        $sql = "SELECT * 
+                FROM portfolio_items 
+                WHERE id = :id";
+
+        $db = static::getDB();
+
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+        $stmt->execute();
+        return $stmt->fetch(PDO::FETCH_ASSOC);
     }
 }
