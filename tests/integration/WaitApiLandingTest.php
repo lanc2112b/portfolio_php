3677c342f1d4074a301e93c4d6a330896c692a04
Tony Lancaster <lanc1968@gmail.com>
2023-04-13 18:53:58 +0100

    Add test for get /api/landing.

--- tests/integration/WaitApiLandingTest.php
+++ tests/integration/WaitApiLandingTest.php
@@ -3,22 +3,20 @@
 use PHPUnit\Framework\TestCase;
 use GuzzleHttp\Client;
 
-use function PHPUnit\Framework\assertArrayHasKey;
-
 class WaitApiLandingTest extends TestCase
 {
 
     protected $client;
 
     protected $post_data = [
-        'area_title' => 'main',
+        'area_title' => 'tertiary',
         'area_content_title' => 'Some main page title',
         'area_content' => 'Lots of content',
         'area_content_image' => 'https://news.muninn.co.uk/carousel_imgs/react_slide.png',
     ];
 
     protected $patch_data = [
-        'area_title' => 'main',
+        'area_title' => 'tertiary',
         'area_content_title' => 'A Better Main Area Title',
         'area_content' => 'Should really put some lorem ipsum here',
         'area_content_image' => 'https://news.muninn.co.uk/carousel_imgs/node_slide.png',
@@ -32,5 +30,25 @@ class WaitApiLandingTest extends TestCase
             'verify' => false,
             'headers' => ['Content-Type' => 'application/json']
         ]);
+    }
+
+    public function testGetLandingsItems()
+    {
+
+        $response = $this->client->request('GET', '/api/landings/index');
+
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $data = json_decode($response->getBody(), true);
+
+        $this->assertIsArray($data);
+        $this->assertCount(2, $data);
+        $this->assertArrayHasKey('id', $data[0]);
+        $this->assertArrayHasKey('area_title', $data[0]);
+        $this->assertArrayHasKey('area_content_title', $data[0]);
+        $this->assertArrayHasKey('area_content', $data[0]);
+        $this->assertArrayHasKey('area_content_image', $data[0]);
+        $this->assertEquals(1, $data[0]['id']);
+        $this->assertEquals(2, $data[1]['id']);
     }
 }
