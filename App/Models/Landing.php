a85e1325ccecf62f1973f31ea1fc743c2201cee2
Tony Lancaster <lanc1968@gmail.com>
2023-04-14 11:14:04 +0100

    Add ep, add action & model method for add landing page content. Add tests for add landing page content. Closes # 31 Add ep, patch action & model method for updating landing page content. Add tests for updating landing page content. Closes #32 Add ep, delete action & model method for deleting landing page content. Add tests for deleting landing page content. Closes #33

--- App/Models/Landing.php
+++ App/Models/Landing.php
@@ -23,15 +23,116 @@ class Landing extends \Core\Model
 
     public function getAll()
     {
-
+        
+        $db = static::getDB();
+        
         $sql = "SELECT * 
                 FROM landing_page";
-
-        $db = static::getDB();
-
+        
         $stmt = $db->query($sql);
 
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
+    public function addItem()
+    {
+        $this->validate();
+
+        if (empty($this->errors)) {
+
+            $sql = 'INSERT INTO landing_page
+                (area_title, area_content_title, area_content, area_content_image)
+                VALUES
+                (:area_title, :area_content_title, :area_content, :area_content_image)
+                RETURNING *';
+
+            $db = static::getDB();
+            $stmt = $db->prepare($sql);
+
+            $stmt->bindValue(':area_title', $this->area_title, PDO::PARAM_STR);
+            $stmt->bindValue(':area_content_title', $this->area_content_title, PDO::PARAM_STR);
+            $stmt->bindValue(':area_content', $this->area_content, PDO::PARAM_STR);
+            $stmt->bindValue(':area_content_image', $this->area_content_image, PDO::PARAM_STR);
+
+            $stmt->execute();
+
+            return $stmt->fetch(PDO::FETCH_ASSOC);
+        }
+        return false;
+    }
+
+    public function updateItemById($id)
+    {
+        $this->validate();
+
+        if (!is_numeric($id)) {
+            $this->errors[] = 'Id MUST be a number.';
+        }
+
+        if (empty($this->errors)) {
+            
+            $sql = 'UPDATE landing_page
+                    SET
+                    area_title = :area_title,
+                    area_content_title = :area_content_title,
+                    area_content = :area_content,
+                    area_content_image = :area_content_image
+                    WHERE id = :id';
+
+            $db = static::getDB();
+            $stmt = $db->prepare($sql);
+
+            $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+            $stmt->bindValue(':area_title', $this->area_title, PDO::PARAM_STR);
+            $stmt->bindValue(':area_content_title', $this->area_content_title, PDO::PARAM_STR);
+            $stmt->bindValue(':area_content', $this->area_content, PDO::PARAM_STR);
+            $stmt->bindValue(':area_content_image', $this->area_content_image, PDO::PARAM_STR);
+
+            $stmt->execute();
+
+            return $stmt->fetch(PDO::FETCH_ASSOC);
+        }
+        return false;
+    }
+
+    public function deleteItemById($id)
+    {
+        if (!is_numeric($id)) {
+            $this->errors[] = 'Id MUST be a number.';
+        }
+
+        if (empty($this->errors)) {
+
+            $db = static::getDB();
+
+            $sql = "DELETE 
+                FROM landing_page 
+                WHERE id = :id";
+
+            $stmt = $db->prepare($sql);
+            $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+
+            return $stmt->execute();
+        }
+    }
+
+    public function validate()
+    {
+
+        if ($this->area_title == '' || strlen($this->area_title) < 3 || strlen($this->area_title) > 24) {
+            $this->errors[] = 'Area title must be over 3 chars & less than 24';
+        }
+
+        if ($this->area_content_title == '' || strlen($this->area_content_title) < 12 || strlen($this->area_content_title) > 254) {
+            $this->errors[] = 'Content title must be over 12 chars & less than 254';
+        }
+
+        if ($this->area_content == ''|| strlen($this->area_content) < 20 || strlen($this->area_content) > 3000) {
+            $this->errors[] = 'Content must be over 20 chars & less than 3000';
+        }
+
+        if ($this->area_content_image == '' || !filter_var($this->area_content_image, FILTER_VALIDATE_URL, FILTER_FLAG_PATH_REQUIRED) || !getimagesize($this->area_content_image)  || strlen($this->area_content_image) > 254) {
+            $this->errors[] = 'The provided url is not valid for content image';
+        }
+    }
 }
--- tests/integration/WaitApiLandingTest.php
+++ tests/integration/WaitApiLandingTest.php
@@ -10,8 +10,8 @@ class WaitApiLandingTest extends TestCase
 
     protected $post_data = [
         'area_title' => 'tertiary',
-        'area_content_title' => 'Some main page title',
-        'area_content' => 'Lots of content',
+        'area_content_title' => 'Some main page title.',
+        'area_content' => 'Lots of content, more than 20 chars',
         'area_content_image' => 'https://news.muninn.co.uk/carousel_imgs/react_slide.png',
     ];
 
@@ -50,5 +50,74 @@ class WaitApiLandingTest extends TestCase
         $this->assertArrayHasKey('area_content_image', $data[0]);
         $this->assertEquals(1, $data[0]['id']);
         $this->assertEquals(2, $data[1]['id']);
+    }
+
+    public function testPostLandingContent()
+    {
+
+        $response = $this->client->request(
+            'POST',
+            '/api/admin/landings/add',
+            ['body' => json_encode($this->post_data)]
+        );
+
+        $this->assertEquals(201, $response->getStatusCode());
+
+        $data = json_decode($response->getBody(), true);
+
+        $this->assertIsArray($data);
+        $this->assertCount(1, $data);
+        $this->assertArrayHasKey('item', $data);
+        $this->assertCount(8, $data['item']);
+        $this->assertArrayHasKey('area_title', $data['item']);
+        $this->assertEquals('tertiary', $data['item']['area_title']);
+        $this->assertEquals('Some main page title.', $data['item']['area_content_title']);
+        $this->assertEquals('Lots of content, more than 20 chars', $data['item']['area_content']);
+        $this->assertEquals('https://news.muninn.co.uk/carousel_imgs/react_slide.png', $data['item']['area_content_image']);
+    }
+
+    public function testPostBadLandingContent()
+    {
+
+        $response = $this->client->request(
+            'POST',
+            '/api/admin/landings/add',
+            ['http_errors' => false],
+            ['body' => json_encode(['area_title' => 'we'])],
+
+        );
+
+        $this->assertEquals(400, $response->getStatusCode());
+
+        $data = json_decode($response->getBody(), true);
+
+        $this->assertIsArray($data);
+        $this->assertEquals('Bad request', $data['msg']);
+    }
+
+    public function testPatchedLandingContent()
+    {
+
+        /* var_dump($this->patch_data);
+        ob_flush(); */
+        $response = $this->client->request(
+            'PATCH',
+            '/api/admin/landings/3/update',
+            ['body' => json_encode($this->patch_data)]
+        );
+
+        $this->assertEquals(204, $response->getStatusCode());
+    }
+
+    public function testDeleteLandingContent()
+    {
+
+        $response = $this->client->request(
+            'DELETE',
+            '/api/admin/landings/3/delete',
+            ['body' => json_encode($this->patch_data)]
+        );
+
+        $this->assertEquals(204, $response->getStatusCode());
     }
 }
