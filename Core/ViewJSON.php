f47a616abbd25859235a050fba931fbd7c16e684
Tony Lancaster <lanc1968@gmail.com>
2023-04-15 19:02:43 +0100

    Add CORS access.

--- Core/ViewJSON.php
+++ Core/ViewJSON.php
@@ -14,6 +14,9 @@ class ViewJSON
     public static function responseJson($response, $status = 200)
     {
         header_remove();
+        //header("Access-Control-Allow-Origin: *");  // works
+        header("Access-Control-Allow-Origin: http://localhost:3000"); // works
+        header("Access-Control-Allow-Headers: Content-Type");
         header('Content-Type: application/json');
         http_response_code($status);
         echo json_encode($response);
