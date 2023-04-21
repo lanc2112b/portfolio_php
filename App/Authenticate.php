82a4f5bb4f2d1c38161a4f40a40db7b4c426e72a
Tony Lancaster <lanc1968@gmail.com>
2023-04-21 12:52:53 +0100

    Change no auth errors to 401.

--- App/Authenticate.php
+++ App/Authenticate.php
@@ -12,11 +12,11 @@ class Authenticate
     {
 
         if(!array_key_exists('HTTP_AUTHORIZATION', $_SERVER)){
-            throw new \Exception('No authorization provided', 403);
+            throw new \Exception('No authorization provided', 401);
         }
 
         if (!preg_match('/Bearer\s(\S+)/', $_SERVER['HTTP_AUTHORIZATION'], $matches)) {
-            throw new \Exception('No token received', 403);
+            throw new \Exception('No token received', 401);
         }
 
         $mdl = new User(['credential' => $matches[1]]);
