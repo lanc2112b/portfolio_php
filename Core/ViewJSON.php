e9cd924131bbe0dd00edb99885b4661a21a1ad9a
Tony Lancaster <lanc1968@gmail.com>
2023-04-29 11:18:37 +0100

    Add functionality to detect a valid request host. Alters CORS ..allow-origin to deliver to 1 of 2 valid requesters.

--- Core/ViewJSON.php
+++ Core/ViewJSON.php
@@ -10,12 +10,27 @@ namespace Core;
 class ViewJSON
 {
 
+    /* protected $allowed_hosts = ['https://www.muninn.co.uk', 'https://pap.muninn.co.uk'];
+    protected $http_origin; */
+
+    
+
     /** FIXME: status codes.... */
     public static function responseJson($response, $status = 200)
     {
+
+        $allowed_hosts = ['https://www.muninn.co.uk', 'https://pap.muninn.co.uk'];
+
+        $http_origin = '';
+
+        if (in_array($_SERVER['HTTP_ORIGIN'], $allowed_hosts)) {
+            $http_origin = $_SERVER['HTTP_ORIGIN'];
+        }
+
+
         header_remove();
         //header("Access-Control-Allow-Origin: *");  // works
-        header("Access-Control-Allow-Origin: http://localhost:3000"); // works
+        header("Access-Control-Allow-Origin: $http_origin"); // works
         header("Access-Control-Allow-Headers: Content-Type, Authorization");
         header('Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PATCH');
         header('Access-Control-Allow-Credentials: true');
@@ -23,6 +38,15 @@ class ViewJSON
         http_response_code($status);
         echo json_encode($response);
     }
+
+
+/*     private function getOrigin() {
+
+        if(in_array($_SERVER['HTTP_ORIGIN'], $this->allowed_hosts)) {
+            $this->http_origin = $_SERVER['HTTP_ORIGIN'];
+        }
+
+    } */
 
 
 
