0fbfb35852dac43ffef04ef85c99ffe0dfde48ee
Tony Lancaster <lanc1968@gmail.com>
2023-05-15 18:48:04 +0100

    Allowed CORS domains moved to .env

--- Core/ViewJSON.php
+++ Core/ViewJSON.php
@@ -10,17 +10,11 @@ namespace Core;
 class ViewJSON
 {
 
-    /* protected $allowed_hosts = ['https://www.muninn.co.uk', 'https://pap.muninn.co.uk'];
-    protected $http_origin; */
-
-    
-
     /** FIXME: status codes.... */
     public static function responseJson($response, $status = 200)
     {
 
-        $allowed_hosts = ['https://www.muninn.co.uk', 'https://pap.muninn.co.uk'];
-        //$allowed_hosts = ['https://www.muninn.co.uk', 'https://pap.muninn.co.uk', 'http://localhost:3000'];
+        $allowed_hosts = explode(',', $_ENV['ALLOWED_HOSTS']);
 
         $http_origin = '';
 
@@ -30,8 +24,7 @@ class ViewJSON
 
 
         header_remove();
-        //header("Access-Control-Allow-Origin: *");  // works
-        header("Access-Control-Allow-Origin: $http_origin"); // works
+        header("Access-Control-Allow-Origin: $http_origin");
         header("Access-Control-Allow-Headers: Content-Type, Authorization");
         header('Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PATCH');
         header('Access-Control-Allow-Credentials: true');
@@ -39,16 +32,4 @@ class ViewJSON
         http_response_code($status);
         echo json_encode($response);
     }
-
-
-/*     private function getOrigin() {
-
-        if(in_array($_SERVER['HTTP_ORIGIN'], $this->allowed_hosts)) {
-            $this->http_origin = $_SERVER['HTTP_ORIGIN'];
-        }
-
-    } */
-
-
-
 }
