7b10c3b9622328980d10a7f6534866adb5c66782
Tony Lancaster <lanc1968@gmail.com>
2023-05-31 16:16:02 +0100

    refactor responseJson

--- Core/ViewJSON.php
+++ Core/ViewJSON.php
@@ -5,13 +5,13 @@ namespace Core;
 /** JSON response 
  * 
  * Perhaps needs moving to an API folder? 
-*/
+ */
 
 class ViewJSON
 {
 
     /** FIXME: status codes.... */
-    public static function responseJson($response, $status = 200)
+    public static function responseJson($response,  $status = 200, $cookie = []) //$refresh_token = null,
     {
 
         $allowed_hosts = explode(',', $_ENV['ALLOWED_HOSTS']);
@@ -22,13 +22,24 @@ class ViewJSON
             $http_origin = $_SERVER['HTTP_ORIGIN'];
         }
 
-
         header_remove();
         header("Access-Control-Allow-Origin: $http_origin");
         header("Access-Control-Allow-Headers: Content-Type, Authorization");
         header('Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PATCH');
         header('Access-Control-Allow-Credentials: true');
         header('Content-Type: application/json');
+        if (!empty($cookie)) {
+            setcookie(
+                "jwt",
+                $cookie['token'],
+                [
+                    "expires" => $cookie['expiry'],
+                    "httpOnly" => true,
+                    "secure" => true,
+                    "samesite" => 'None',
+                ]
+            );
+        }
         http_response_code($status);
         echo json_encode($response);
     }
