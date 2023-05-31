28f5b37121947b5eda88cc26ab1f1a4c8f07bdf1
Tony Lancaster <lanc1968@gmail.com>
2023-05-31 16:14:58 +0100

    refactor constructor refactor createUser refactor validateUserLogin add saveRefreshToken refactor validateAuthorisedUser add verifyRefreshToken add verifyAccessToken refactor verifyToken rename verifyToken -> verifyGoogleToken add getUserByEmail add getUserByRefreshToken refactor getUserByGoogleID refactor saveUser add destroyRefreshToken

--- App/Models/User.php
+++ App/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Core\Model;
+use Core\Tokens;
 use Google_Client;
 use DateTime;
 use PDO;
@@ -14,10 +15,12 @@ class User extends Model
     protected $client;
 
     protected $credential;
-    protected $payload;
+
+    protected $google_payload;
 
     protected $user;
 
+    protected $cookie_refresh;
 
     public function __construct($data = [])
     {
@@ -25,99 +28,248 @@ class User extends Model
             $this->$key = $value;
         };
 
-        $this->client = new Google_Client(['client_id' => $_ENV['GOOGLE_CLIENT_ID']]);
+        if (isset($_COOKIE['jwt']))
+            $this->cookie_refresh = $_COOKIE['jwt'];
 
+        $this->client = new Google_Client(['client_id' => $_ENV['GOOGLE_CLIENT_ID']]);
     }
 
     public function createUser()
     {
-        if (!$this->verifyToken())
-            return ['msg' => 'Invalid token or user'];
+        if (!$this->verifyGoogleToken())
+            return ['msg' => 'Invalid token'];
 
         if ($this->getUserByGoogleID())
             return ['msg' => 'User already exists'];
 
-        if ($this->user = $this->saveUser())
-            return $this->user;
+        if (!$this->saveUser())
+            return ['msg' => 'Failed creating user'];
 
-        return [];
+        $this->user = $this->getUserByGoogleID();
+
+        $tkn = new Tokens();
+
+        $refresh_info = $tkn->getNewJWTToken('refresh', $this->user['email']);
+        $access_info = $tkn->getNewJWTToken('access', $this->user['email']);
+
+        $this->saveRefreshToken($this->user['email'], $refresh_info);
+
+        $user = array_merge($this->user['email'], $access_info);
+
+        return ['user' => $user, 'refresh_info' => $refresh_info];
     }
 
     public function validateUserLogin()
     {
-        if (!$this->verifyToken())
+        if (!$this->verifyGoogleToken()) // google JWT
             return ['msg' => 'Invalid token or user'];
 
-        $validUser = $this->getUserByGoogleID();
-        if(!$validUser)
+        $valid_user = $this->getUserByGoogleID();
+
+        if (!$valid_user)
             return ['msg' => 'User not found'];
 
-        if($validUser['refresh_at'] <= date("Y-m-d H:i:s")) {
-            // update user
-            $this->updateUser();
-        }
+        $tkn = new Tokens();
 
-        return $this->getUserByGoogleID();
+        $refresh_info = $tkn->getNewJWTToken('refresh', $valid_user['email']);
+        $access_info = $tkn->getNewJWTToken('access', $valid_user['email']);
+
+        $this->saveRefreshToken($valid_user['email'], $refresh_info);
+
+        $user = array_merge($valid_user, $access_info);
+
+        return ['user' => $user, 'refresh_info' => $refresh_info];
+    }
+
+    private function saveRefreshToken($email, $token_info)
+    {
+
+        $sql = 'UPDATE users
+                SET
+                refresh_token = :refresh_token,
+                refresh_at = :refresh_at
+                WHERE 
+                email = :email';
+
+        $db = static::getDB();
+        $stmt = $db->prepare($sql);
+
+        $stmt->bindValue(':email', $email, PDO::PARAM_STR);
+        $stmt->bindValue(':refresh_token', $token_info['token'], PDO::PARAM_STR);
+        $stmt->bindValue(':refresh_at', date("Y-m-d H:i:s", $token_info['expiry']), PDO::PARAM_STR);
+
+        return $stmt->execute();
     }
 
     public function validateAuthorisedUser()
     {
-        if (!$this->verifyToken())
-            return ['msg' => 'Invalid token or user'];
 
-        $validUser = $this->getUserPermsByGoogleID();
-        if (!$validUser)
-            return ['msg' => 'User not found'];
+        $decoded = $this->verifyAccessToken();
 
-        if(!$validUser['is_admin'])
+        if (method_exists($decoded, 'getMessage')) {
+            if ($decoded->getMessage() === 'Expired token') {
+
+                return ['msg' => 'Expired token'];
+            } else {
+
+                return ['msg' => 'Invalid token'];
+            }
+        }
+
+        if ($decoded->iss !== $_ENV['DOMAIN'] || $decoded->aud !== $_ENV['AUDIENCE'])
+            return ['msg' => 'Invalid token'];
+
+
+        if (property_exists($decoded, 'user')) {
+
+            $user = $this->getUserByEmail($decoded->user);
+
+            if (!$user)
+                return ['msg' => 'No user found'];
+
+            if (!$user['is_admin'])
+                return ['msg' => 'Unauthorised user'];
+        } else {
+
             return ['msg' => 'Unauthorised user'];
+        }
 
-        //$str = $validUser['refresh_at'] . " vs. " . date('Y-m-d H:i:s');
-        if ($validUser['refresh_at'] <= date("Y-m-d H:i:s"))
-            return ['msg' => "Token expired"];
-
-        return ['msg' => true];
-
+        return ['msg' => 'Valid user'];
     }
 
-    private function verifyToken()
+    public function getNewAccessToken()
     {
-        $this->payload = $this->client->verifyIdToken($this->credential);
 
-        if ($this->payload && $this->payload['aud'] === $_ENV['GOOGLE_CLIENT_ID'])           
+        $this->user = $this->getUserByRefreshToken();
+
+        if (!$this->user)
+            return ['msg' => 'Invalid token'];
+
+        $decoded = $this->verifyRefreshToken();
+
+        if (method_exists($decoded, 'getMessage')) {
+
+            if ($decoded->getMessage() === 'Expired token') {
+
+                return ['msg' => 'Expired token'];
+            }
+
+            return ['msg' => 'Invalid token'];
+        }
+
+        if ($decoded->iss !== $_ENV['DOMAIN'] || $decoded->aud !== $_ENV['AUDIENCE'])
+            return ['msg' => 'Invalid token'];
+
+        if (property_exists($decoded, 'user')) {
+
+            if ($decoded->user !== $this->user['email'])
+                return ['msg' => 'Invalid token'];
+
+            if (!$this->user['is_admin'])
+                return ['msg' => 'Unauthorised user'];
+        } else {
+
+            return ['msg' => 'Invalid token'];
+        }
+
+        $tkn = new Tokens();
+
+        $token = $tkn->getNewJWTToken('access', $this->user['email']);
+
+        unset($this->user['is_admin']);
+
+        $refresh_array = array_merge($this->user, $token);
+
+        return ['auth' => $refresh_array];
+    }
+
+
+    private function verifyRefreshToken()
+    {
+
+        $tkn = new Tokens();
+
+        try {
+
+            $decoded = $tkn->decodeRefreshToken($this->cookie_refresh);
+
+            return $decoded;
+        } catch (\Exception $e) {
+
+            return $e;
+        }
+    }
+
+
+    private function verifyAccessToken()
+    {
+
+        $tkn = new Tokens();
+
+        try {
+
+            $decoded = $tkn->decodeAccessToken($this->credential);
+
+            return $decoded;
+        } catch (\Exception $e) {
+
+            return $e;
+        }
+    }
+
+    private function verifyGoogleToken()
+    {
+        $this->google_payload = $this->client->verifyIdToken($this->credential);
+
+        if ($this->google_payload && $this->google_payload['aud'] === $_ENV['GOOGLE_CLIENT_ID'])
             return true;
 
         return false;
     }
 
-    private function getUserByGoogleID()
+    private function getUserByEmail($email)
     {
-        
-        $gid = $this->payload['sub'];
 
-  
-        $sql = "SELECT gid, first_name, last_name, email, photo_url, access_token, refresh_at 
+        $sql = "SELECT first_name, last_name, email, photo_url, is_admin 
                 FROM users 
-                WHERE gid = :gid";
+                WHERE email = :email";
 
         $db = static::getDB();
 
         $stmt = $db->prepare($sql);
 
-        $stmt->bindParam(':gid', $gid, PDO::PARAM_STR);
+        $stmt->bindParam(':email', $email, PDO::PARAM_STR);
 
         $stmt->execute();
 
         return $stmt->fetch(PDO::FETCH_ASSOC);
     }
 
-    private function getUserPermsByGoogleID()
+    private function getUserByRefreshToken()
     {
 
-        $gid = $this->payload['sub'];
+        $sql = "SELECT first_name, last_name, email, photo_url, is_admin 
+                FROM users 
+                WHERE refresh_token = :token";
+
+        $db = static::getDB();
+
+        $stmt = $db->prepare($sql);
+
+        $stmt->bindParam(':token', $this->cookie_refresh, PDO::PARAM_STR);
+
+        $stmt->execute();
+
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    private function getUserByGoogleID()
+    {
+
+        $gid = $this->google_payload['sub'];
 
 
-        $sql = "SELECT is_admin, refresh_at 
+        $sql = "SELECT first_name, last_name, email, photo_url 
                 FROM users 
                 WHERE gid = :gid";
 
@@ -134,79 +286,53 @@ class User extends Model
 
     private function saveUser()
     {
-        //$this->validate();
 
-        //if (empty($this->errors)) {
-
-            $sql = 'INSERT INTO users
-                (gid, first_name, last_name, photo_url, email, access_token, refresh_at)
+        $sql = 'INSERT INTO users
+                (gid, first_name, last_name, photo_url, email, refresh_token, refresh_at)
                 VALUES
-                (:gid, :first_name, :last_name, :photo_url, :email, :access_token, :refresh_at)
-                '; //RETURNING gid, first_name, last_name, email, photo_url, access_token, refresh_at
-
-            $db = static::getDB();
-            $stmt = $db->prepare($sql);
-
-            $stmt->bindValue(':gid', $this->payload['sub'], PDO::PARAM_STR);
-            $stmt->bindValue(':first_name', $this->payload['given_name'], PDO::PARAM_STR);
-            $stmt->bindValue(':last_name', $this->payload['family_name'], PDO::PARAM_STR);
-            $stmt->bindValue(':photo_url', $this->payload['picture'], PDO::PARAM_STR);
-            $stmt->bindValue(':email', $this->payload['email'], PDO::PARAM_STR);
-            $stmt->bindValue(':access_token', $this->credential, PDO::PARAM_STR);
-            $stmt->bindValue(':refresh_at', date("Y-m-d H:i:s", $this->payload['exp']), PDO::PARAM_STR);
-        
-            return $stmt->execute();
-
-            //return $stmt->fetch(PDO::FETCH_ASSOC);
-        //}
-        //return false;
-    }
-
-    private function updateUser(){
-
-        $sql = 'UPDATE users
-                SET
-                access_token = :access_token,
-                refresh_at = :refresh_at
-                WHERE 
-                gid = :gid';
+                (:gid, :first_name, :last_name, :photo_url, :email, :refresh_token, :refresh_at)';
 
         $db = static::getDB();
         $stmt = $db->prepare($sql);
 
-        $stmt->bindValue(':gid', $this->payload['sub'], PDO::PARAM_STR);
-        $stmt->bindValue(':access_token', $this->credential, PDO::PARAM_STR);
-        $stmt->bindValue(':refresh_at', date("Y-m-d H:i:s", $this->payload['exp']), PDO::PARAM_STR);
+        $stmt->bindValue(':gid', $this->google_payload['sub'], PDO::PARAM_STR);
+        $stmt->bindValue(':first_name', $this->google_payload['given_name'], PDO::PARAM_STR);
+        $stmt->bindValue(':last_name', $this->google_payload['family_name'], PDO::PARAM_STR);
+        $stmt->bindValue(':photo_url', $this->google_payload['picture'], PDO::PARAM_STR);
+        $stmt->bindValue(':email', $this->google_payload['email'], PDO::PARAM_STR);
+        $stmt->bindValue(':refresh_token', $this->credential, PDO::PARAM_STR);
+        $stmt->bindValue(':refresh_at', date("Y-m-d H:i:s", $this->google_payload['exp']), PDO::PARAM_STR);
 
-        $stmt->execute();
-
-        return $stmt->fetch(PDO::FETCH_ASSOC);
-
+        return $stmt->execute();
     }
 
- /*    public function validate()
+    public function destroyRefreshToken()
     {
 
-        if (
-            $this->name == '' || strlen($this->name) < 10 || strlen($this->name) > 254
-        ) {
-            $this->errors[] = 'A fullname is required, between 10 & 254 chars';
-        }
+        if ($this->verifyRefreshToken()) {
 
-        if ($this->email == '' || !filter_var($this->email, FILTER_VALIDATE_EMAIL) || strlen($this->email) > 254) {
-            $this->errors[] = 'A valid email is required, max 254 chars';
-        }
+            $this->user = $this->getUserByRefreshToken();
 
-        if ($this->subject == '' || strlen($this->subject) < 10 || strlen($this->subject) > 254) {
-            $this->errors[] = 'A subject is required, between 10 & 254 chars';
-        }
+            if (!$this->user)
+                return ['msg' => 'Invalid token'];
 
-        if ($this->query == '' || strlen($this->query) < 10 || strlen($this->query) > 1500) {
-            $this->errors[] = 'A subject is required, between 10 & 1500 chars';
-        }
+            if ($this->user['email']) {
 
-        if ($this->source == '' || strlen($this->source) < 10  || strlen($this->source) > 254) {
-            $this->errors[] = 'A source is required, between 10 & 254 chars';
+                $sql = 'UPDATE users
+                SET
+                refresh_token = NULL,
+                refresh_at = :refresh_at
+                WHERE 
+                email = :email';
+
+                $db = static::getDB();
+                $stmt = $db->prepare($sql);
+
+                $stmt->bindValue(':email', $this->user['email'], PDO::PARAM_STR);
+                $stmt->bindValue(':refresh_at', date("Y-m-d H:i:s", time()), PDO::PARAM_STR);
+
+                return $stmt->execute();
+            }
         }
-    } */
+    }
 }
